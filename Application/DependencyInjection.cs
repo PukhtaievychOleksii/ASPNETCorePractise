5c2f2746652acfcf0db69c301603ee0823a187f1
Oleksii Pukhtaievych <pukhtaievych.oleksii@gmail.com>
2024-04-16 00:56:50 +0200

    DomainDesignTemplate

---
+++ Application/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddAplication(this IServiceCollection services)
+        {
+            var assembley = typeof(DependencyInjection).Assembly;
+
+            services.AddMediatR(configuration =>
+                configuration.RegisterServicesFromAssembly(assembley));
+
+            services.AddValidatorsFromAssembly(assembley);
+
+            return services;
+        }
+    }
+}
